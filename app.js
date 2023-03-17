dbe8e57abdc9032c398c082a0f33bbdf6d6a2099
Kevin <andrew.koomson@amalitech.org>
2023-03-17 10:40:23 +0000

    increasing file upload limit

--- app.js
+++ app.js
@@ -75,7 +75,7 @@ var storage = multer.memoryStorage();
 var upload = multer({
     storage: storage,
     limits: {
-        fileSize: 5 * 1024 * 1024
+        fileSize: 10 * 1024 * 1024
     }
 });
 app.use(bodyParser.urlencoded({
--- app.ts
+++ app.ts
@@ -45,7 +45,7 @@ const storage = multer.memoryStorage();
 const upload = multer({
     storage: storage,
     limits: {
-        fileSize: 5 * 1024 * 1024
+        fileSize: 10 * 1024 * 1024
     }
 })
 
