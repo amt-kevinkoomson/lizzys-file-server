988e982ccf9d2794e6928aa3f93e2521790a7997
Kevin <andrew.koomson@amalitech.org>
2023-03-23 15:26:03 +0000

    Replacing database information to host compatible

--- models/database.ts
+++ models/database.ts
@@ -4,12 +4,12 @@ const {
 } = require('pg');
 
 const client = new Client({
-    // ssl: { rejectUnauthorized: false },
-    user: 'kevo',
-    password: 'phase3proj',
-    host: 'localhost',
-    port: 5432,
-    database: 'phase3'
+    ssl: { rejectUnauthorized: false },
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_DATABASE
 });
 
 try {
